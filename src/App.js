12360ea9a540d02037bd471ac105b27e8f956f41
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-09-06 21:10:28 +0100

    Design the first element in the component that sends the http request

--- src/App.js
+++ src/App.js
@@ -1,21 +1,85 @@
-import React from 'react';
+// import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import FormControl from '@material-ui/core/FormControl';
+// import Select from '@material-ui/core/Select';
 import './App.css';
-import Axios from 'axios';
+
+const METHODS = [
+	'GET',
+	'POST',
+	'PUT',
+	'DELETE',
+	'PATCH',
+	'HEAD',
+	'OPTIONS',
+	'CONNECT',
+	'TRACE',
+];
 
 function App() {
-	const onClick = () => {
-		Axios.get('https://jsonplaceholder.typicode.com/todos/').then(res =>
-			console.log(res.data)
-		);
-	};
+	// const [res, setRes] = useState();
+	// const [err, setErr] = useState();
+	// useEffect(() => {
+	// 	console.log(res);
+	// }, [res]);
+	// const onClick = async () => {
+	// 	console.clear();
+	// 	try {
+	// 		const response = await Axios({
+	// 			url: 'todos/',
+	// 			data: 'yeah!',
+	// 			baseURL: 'https://jsonplaceholder.typicode.com',
+	// 			// validateStatus: status =>true,
+	// 		});
+	// 		setRes(response);
+	// 	} catch (error) {
+	// 		setErr(error);
+	// 		console.log({ err });
+	// 	}
+	// };
 
 	return (
-		<div className='app'>
-			<h1>App</h1>
-			<textarea name='test' id='test' defaultValue='test'></textarea>
-			<br />
-			<button onClick={onClick}>Send</button>
-		</div>
+		<main className='app'>
+			<header>
+				<h1>Post Women</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+					repudiandae eius distinctio laboriosam enim quo animi eum recusandae
+					eveniet voluptatum, impedit dolor. Impedit laudantium minus deleniti
+					ut dignissimos totam quo!
+				</p>
+			</header>
+			<article className='row'>
+				<div className='inputs'>
+					<section className='request'>
+						<div className='intro'>
+							<input
+								name='method'
+								list='methods'
+								className='methods'
+								defaultValue={METHODS[0]}
+							/>
+							<datalist id='methods'>
+								{METHODS.map(method => {
+									return (
+										<option key={method} value={method}>
+											{method}
+										</option>
+									);
+								})}
+							</datalist>
+							<textarea name='url' id='url' rows='1' className='url' />
+							<button className='send'>Send</button>
+						</div>
+					</section>
+					<section className='response'>res</section>
+				</div>
+				<article className='logs'>logs</article>
+			</article>
+			<footer>elaroussi.me</footer>
+		</main>
 	);
 }
 
