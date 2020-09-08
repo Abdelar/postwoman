bf7984c32d41f621bc8db0a05577398dd76c8278
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-09-09 00:27:03 +0100

    Updates

---
+++ precache-manifest.26e3a316787dc53aba21901ffcc975b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e25b83e6160c5475bdbb6d6ab83c4c8",
+    "url": "/postwomen/index.html"
+  },
+  {
+    "revision": "afb9d7984f60e7cfe109",
+    "url": "/postwomen/static/css/main.9fab8094.chunk.css"
+  },
+  {
+    "revision": "e68ee6221d27e8d48ba9",
+    "url": "/postwomen/static/js/2.06594954.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/postwomen/static/js/2.06594954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb9d7984f60e7cfe109",
+    "url": "/postwomen/static/js/main.38465635.chunk.js"
+  },
+  {
+    "revision": "aba0ab5851f2008554d8",
+    "url": "/postwomen/static/js/runtime-main.e6655ded.js"
+  }
+]);
