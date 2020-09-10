fc98d071c92f8663175566a5050f11ee733bf2ce
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-09-10 23:11:06 +0100

    Update the log section

--- src/component/Logs.js
+++ src/component/Logs.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Logs.css';
 
-export const Logs = () => {
+export const Logs = props => {
 	return (
 		<article className='logs'>
 			<h2>Logs</h2>
+			<div className='logs_body'>
+				<h3>Wow, such empty</h3>
+			</div>
 		</article>
 	);
 };
